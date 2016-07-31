4a512bcbab1436f91b4602f81ee3eedf149fc31d
suya1122@gmail.com <suya1122@gmail.com>
2016-07-31 13:48:45 -0400

    factorial test 4 pass

--- src/main/java/com/thoughtworks/tddintro/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/factorial/Factorial.java
@@ -1,10 +1,12 @@
 package com.thoughtworks.tddintro.factorial;
 
 public class Factorial {
-    public Integer compute(int i) {
-        if (i != 0)
-            return i;
-        else
-            return 1;
+    public Integer compute(int num) {
+        int factorial = 1;
+        if (num != 0)
+            for (int i = 1; i <= num; i++)
+              factorial *= i;
+
+        return factorial;
     }
 }
