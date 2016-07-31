916b25963cb01c95500b2cce331f02f60fb1b304
suya1122@gmail.com <suya1122@gmail.com>
2016-07-31 14:06:58 -0400

    refactored factorial code

--- src/main/java/com/thoughtworks/tddintro/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/factorial/Factorial.java
@@ -3,13 +3,13 @@ package com.thoughtworks.tddintro.factorial;
 public class Factorial {
     public Integer compute(int num) {
 
-        if  (num == -1)
+        if  (num < 0)
             throw new IllegalArgumentException();
 
         int factorial = 1;
-        if (num != 0)
-            for (int i = 1; i <= num; i++)
-              factorial *= i;
+
+        for (int i = 1; i <= num; i++)
+          factorial *= i;
 
         return factorial;
     }
