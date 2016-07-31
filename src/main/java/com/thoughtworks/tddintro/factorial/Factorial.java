56bf85708996e79c149ef82c6627cffd6c22d08f
suya1122@gmail.com <suya1122@gmail.com>
2016-07-31 13:52:38 -0400

    factorial test 5 pass

--- src/main/java/com/thoughtworks/tddintro/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/factorial/Factorial.java
@@ -2,6 +2,10 @@ package com.thoughtworks.tddintro.factorial;
 
 public class Factorial {
     public Integer compute(int num) {
+
+        if  (num == -1)
+            throw new IllegalArgumentException();
+
         int factorial = 1;
         if (num != 0)
             for (int i = 1; i <= num; i++)
